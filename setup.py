25b9972abe41267507740ec1273f241484231743
VisaCai <35809366+VisaCai@users.noreply.github.com>
2018-01-26 00:08:50 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         ='myfirst11',
+       version      ='1.0.0',
+       py_modules   =['myfirst11'],
+       author       ='Vi_sa',
+       aythor_email ='738507752@qq.com',
+       url          ='https://github.com/VisaCai/Magicbox',
+       description  ='A simple printer of nested lists',
+       )
